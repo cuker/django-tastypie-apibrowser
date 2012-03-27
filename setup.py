c71cb777ec89ec3124028426b2f8b73b7995dcf7
Kevin Mooney <kevin@cukerinteractive.com>
2012-03-26 21:12:18 -0500

    Moved directories around and made setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+setup(
+    name='django-tastypie-apibrowser',
+    version='0.0.1',
+    description='JS Tastypie API Browser for Django.',
+    author='Kevin Mooney',
+    author_email='kevin@webcubecms.com',
+    url='http://github.com/cuker/django-fbapps/',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities'
+    ],
+    test_suite='setuptest.SetupTestSuite',
+    tests_require=(
+        'django-setuptest',
+    ),
+    packages=find_packages(exclude=['test_environment']),
+    include_package_data = True,
+)
+
+
---
+++ tastypie-apibrowser/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+
+urlpatterns = patterns('', 
+    (r'^$', direct_to_template, {'template':'apibrowser/index.html'}, 
+        'api-browser'),
+    )
+
