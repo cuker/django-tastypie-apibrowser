13c00973d30b0d4b4cecb6e078193c8d3cde42c9
Kevin Mooney <kevin@cukerinteractive.com>
2012-03-27 19:46:27 -0500

    Changes to mitigate cross domain problems

--- tastypie_apibrowser/urls.py
+++ tastypie_apibrowser/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls.defaults import *
 from views import staff_member_template
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('', 
-    (r'^$', staff_member_template, {'template':'apibrowser/index.html'}, 
-        'api-browser'),
+    (r'^$',           staff_member_template, 
+                      {'template':'apibrowser/index.html'}, 
+                      'api-browser'),
+    (r'^templates$', direct_to_template, 
+                      {'template':'apibrowser/templates.html'},
+                      'api-browser-templates'),
     )
 
