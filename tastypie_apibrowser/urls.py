8db5873df2ffa21ff6a4fb102f39b9be92095e9b
kmooney <kmooney@gmail.com>
2015-01-06 17:28:58 -0600

    Changes for Django 1.7

--- tastypie_apibrowser/urls.py
+++ tastypie_apibrowser/urls.py
@@ -1,13 +1,21 @@
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls import patterns, url
+except ImportError:
+    from django.conf.urls.defaults import patterns, url
 from views import staff_member_template
-from django.views.generic.simple import direct_to_template
 
-urlpatterns = patterns('', 
-    (r'^$',           staff_member_template, 
-                      {'template':'apibrowser/index.html'}, 
-                      'api-browser'),
-    (r'^templates$', direct_to_template, 
-                      {'template':'apibrowser/templates.html'},
-                      'api-browser-templates'),
+from django.views.generic import TemplateView
+
+urlpatterns = patterns(
+    '',
+    (
+        r'^$',
+        staff_member_template,
+        {'template': 'apibrowser/index.html'}, 'api-browser'
+    ),
+    url(
+        r'^templates$',
+        TemplateView.as_view(template_name='apibrowser/templates.html'),
+        name='api-browser-templates'
     )
-
+)
--- tastypie_apibrowser/views.py
+++ tastypie_apibrowser/views.py
@@ -1,7 +1,7 @@
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 from django.contrib.admin.views.decorators import staff_member_required
 
 @staff_member_required
 def staff_member_template(*args, **kwargs):
-    return direct_to_template(*args, **kwargs)    
+    return TemplateView.as_view(*args, **kwargs)    
 
